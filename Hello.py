877ca5bd47bf05be4e19e80f8169ae5571d27eda
carolinesmith527 <141349033+carolinesmith527@users.noreply.github.com>
2023-08-04 13:49:52 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,63 +1,4 @@
-# # DATE_COLUMN = 'date/time'
-# # DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-# #          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-# # @st.cache_data
-
-# # def load_data(nrows):
-# #     data = pd.read_csv(DATA_URL, nrows=nrows)
-# #     lowercase = lambda x: str(x).lower()
-# #     data.rename(lowercase, axis='columns', inplace=True)
-# #     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-# #     return data
-
-# # # Create a text element and let the reader know the data is loading.
-# # data_load_state = st.text('Loading data...')
-# # # Load 10,000 rows of data into the dataframe.
-# # data = load_data(10000)
-# # # Notify the reader that the data was successfully loaded.
-# # data_load_state.text("Done! (using st.cache_data)")
-
-# # if st.checkbox('Show raw data'):
-# #     st.subheader('Raw data')
-# #     st.write(data)
-
-# # st.subheader('Number of pickups by hour')
-# # hist_values = np.histogram(
-# #     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-# # st.bar_chart(hist_values)
-# # hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
-# # filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-# # st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-# # st.map(filtered_data)
-
-# st.set_page_config(
-#     page_title="Hello",
-#     page_icon="👋",
-# )
-
-# st.write("# Welcome to Streamlit! 👋")
-
-# st.sidebar.success("Select a demo above.")
-
-# st.markdown(
-#     """
-#     Streamlit is an open-source app framework built specifically for
-#     Machine Learning and Data Science projects.
-#     **👈 Select a demo from the sidebar** to see some examples
-#     of what Streamlit can do!
-#     ### Want to learn more?
-#     - Check out [streamlit.io](https://streamlit.io)
-#     - Jump into our [documentation](https://docs.streamlit.io)
-#     - Ask a question in our [community
-#         forums](https://discuss.streamlit.io)
-#     ### See more complex demos
-#     - Use a neural net to [analyze the Udacity Self-driving Car Image
-#         Dataset](https://github.com/streamlit/demo-self-driving)
-#     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-# """
-# )
-# 
 import os
 import pandas as pd
 import csv
@@ -81,7 +22,7 @@ def run_the_app():
         # load the dataset(knowledge base)
         embeddingsdf = pd.read_csv("./data/formatted_corpus.csv")
         st.write('## This is our Corpus:', embeddingsdf[:1000])
-        @st.cache()
+
     except URLError as e:
             st.error(
                 """
@@ -90,7 +31,7 @@ def run_the_app():
             """
                 % e.reason
             )
-
+@st.cache()
 st.markdown(
 """You can input a query or a question. The script then uses semantic search to find relevant passages in Simple English Wikipedia (as it is smaller and fits better in RAM).
 
