186cb540d112e1884178fe0752844ddce7396761
carolinesmith527 <141349033+carolinesmith527@users.noreply.github.com>
2023-08-04 14:43:40 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,57 +1,46 @@
-
-import os
-import pandas as pd
-import csv
-import json
-from sentence_transformers import SentenceTransformer, CrossEncoder, util
-import gzip
-import os
-# import torch
-import os, urllib
-# , cv2
 import streamlit as st
+import helper
 
-# This is the main app app itself, which appears when the user selects "Run the app".
-def run_the_app():
-    # To make Streamlit fast, st.cache allows us to reuse computation across runs.
-    # In this common pattern, we download data from an endpoint only once.
-    
-    # st.write('Importing Data...')
-    try:
-        
-        # load the dataset(knowledge base)
-        embeddingsdf = pd.read_csv("./data/formatted_corpus.csv")
-        st.write('## This is our Corpus:', embeddingsdf[:1000])
+APP_NAME = "Smart Contact Form!"
 
-    except urllib.URLError as e:
-            st.error(
-                """
-                **This demo requires internet access.**
-                Connection error: %s
-            """
-                % e.reason
-            )
-# @st.cache()
-st.markdown(
-"""You can input a query or a question. The script then uses semantic search to find relevant passages in Simple English Wikipedia (as it is smaller and fits better in RAM).
-
-For semantic search, we use SentenceTransformer('multi-qa-MiniLM-L6-cos-v1') and retrieve 32 potentially passages that answer the input query.
-
-Next, we use a more powerful CrossEncoder (cross_encoder = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2')) that scores the query and all retrieved passages for their relevancy. The cross-encoder further boost the performance, especially when you search over a corpus for which the bi-encoder was not trained for."""
+st.set_page_config(
+    page_title=APP_NAME,
+    page_icon=":bar_chart:",
+    layout="wide",
+    initial_sidebar_state="expanded",
 )
 
-# Once we have the dependencies, add a selector for the app mode on the sidebar.
-st.sidebar.title("What to do")
-app_mode = st.sidebar.selectbox("Choose the app mode",
-    ["Show instructions", "Run the app"])
-if app_mode == "Show instructions":
-    st.sidebar.success('To continue select "Run the app".')
-# elif app_mode == "Show the source code":
-#     readme_text.empty()
-#     st.code(get_file_content_as_string("streamlit_app.py"))
-elif app_mode == "Run the app":
-    # readme_text.empty()
-    run_the_app()
-      
-# if __name__ == "__main__":
-#     main()
+st.sidebar.markdown("Made with love using [streamlit](https://streamlit.io/)")
+st.sidebar.image(
+    "images/contact.png"
+)
+
+st.sidebar.title(APP_NAME)
+
+st.header(APP_NAME)
+
+form_expander = st.expander(label='Contact Form')
+with form_expander:
+    sample_queries = [
+        "How to cancel my service?",
+        "new application",
+        "please send my bills"
+    ]       
+    show = st.checkbox("Show Sample Queries")
+    with st.form('Contact Form'):
+        name = st.text_input('Name: ')
+        email = st.text_input('Email Address:')
+        if show:
+            query = st.selectbox('Select Sample Query:', sample_queries, key=1)
+        else:
+            query = st.text_input('Ask Query:', 'Pay Online')
+        submitted = st.form_submit_button('Submit')
+
+if submitted:
+    # show the result response
+    st.subheader("Result")
+    responses = helper.get_query_responses(query, top_k=1)
+    st.json(responses)
+
+    st.subheader("Response")
+    st.success(f"Hello {name},\n{responses[0]['Response']}\nThanks")
